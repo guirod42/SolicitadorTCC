8f7420d457fcd9b4e33b5a92e5893ce722895342
Guilherme Silva Rodrigues <gui.silva.rodrigues@outlook.com.br>
2023-05-23 18:08:41 -0300

    Corrections and creation of professor selection page

--- src/screens/RequestPage/index.tsx
+++ src/screens/RequestPage/index.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { propsStack, propsNavigationStack } from '../../routes/Stack/Models';
 import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
 import { Text } from "react-native";
-import { Container } from "./styles";
+import { Container, List, Title } from "./styles";
+import Professor from "../../components/Professor";
+import { ProfessorDataProps } from "./../../components/Professor/styles"
+import Api from "../../apiService/api";
+import Input from '../../components/Input';
+import { StyledTextInputProps } from "../../components/Input/styles";
 type RequestPageRouteProp = RouteProp<propsNavigationStack, 'RequestPage'>;
 
 const RequestPage = () => {
@@ -10,12 +15,56 @@ const RequestPage = () => {
     const params = useRoute<RequestPageRouteProp>();
     const userid = params.params?.userId;
     const name = params.params?.userName;
+    const [professorsList, setProfessorsList] = useState<ProfessorDataProps[]>([]);
+    const searchInput = React.createRef<StyledTextInputProps>();
+    const [search, setSearch] = useState<string>('');
+    const filteredProfessors = professorsList.filter(
+        (item) => item.nome.toLowerCase().includes(search.toLowerCase())
+    );
+    const noResultsComponent = (
+        <Text style={{ fontStyle: 'italic' }}>Nenhum resultado encontrado</Text>
+    );
+
+    async function searchProfessor() {
+        await Api.get('/usuarios?tipo=2').then((response) => {
+            setProfessorsList(response.data);
+        }).catch(err => console.log(err));
+    }
+
+    useEffect(
+        () => {
+            searchProfessor();
+        },
+        []
+    )
 
     return (
         <Container>
-            <Text>
-                {userid} - {name}
-            </Text>
+            <Title>{'Selecione o professor'}</Title>
+            <Input
+                ref={searchInput}
+                autoCorrect={false}
+                autoCapitalize="none"
+                iconName={"search"}
+                placeholder={"Buscar professor..."}
+                value={search}
+                onChangeText={setSearch}
+            />
+
+            {filteredProfessors.length > 0 ? (
+                <List
+                    data={filteredProfessors}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) =>
+                        <Professor
+                            id={item.id}
+                            nome={item.nome}
+                            imagem={item.imagem}
+                            email={item.email} />
+                    } />
+            )
+                : (noResultsComponent)
+            }
         </Container>
     )
 }
